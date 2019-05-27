b593910b86463f99de9abce27565c083d69e69f5
Parrot Live user <user@localhost.localdomain>
2019-05-27 07:01:02 +0000

    finished landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(window).on("load",function(){
-    $(".loader-wrapper").fadeOut("slow");
-});
+
+
+const menu = document.querySelector('.menu');
+const btn = menu.querySelector('.nav-tgl');
+btn.addEventListener('click', evt => {
+  if (menu.className.indexOf('active', 'nav') === -1) {
+		menu.classList.add('active', 'nav');
+  } else {
+		menu.classList.remove('active', 'nav');
+  }
+})
